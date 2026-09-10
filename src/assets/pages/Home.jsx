5698f1f0f0f86f71bbbcf0dc29814e340fb0410d
aradhana-prasad2244 <aradhanaprasad2210@gmail.com>
2026-09-10 15:06:04 +0530

    Fix FoodDisplay import

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Header from '../../components/Header'
 import MenuCategories from '../../components/MenuCategories'
-import FoodDisplay from '../../components/foodDisplay';
+import FoodDisplay from '../../components/FoodDisplay';
 import AppDownload from '../../components/AppDownload';
 
 const Home = () => {
